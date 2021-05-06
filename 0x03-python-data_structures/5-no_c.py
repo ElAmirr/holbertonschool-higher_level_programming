7d63f59ddadbb788c660bc1d1822460b6ddb1977
ElAmirr <othmaniamir41@gmail.com>
2021-05-07 00:36:37 +0200

    add if stat

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for elm in my_string:
-        if elm != 'c' or elm != 'C':
-            new_string += elm
-    return new_string
+    if my_string:
+        for elm in my_string:
+            if elm != 'c' or elm != 'C':
+                new_string += elm
+        return new_string
+    return
