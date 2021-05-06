46deaa105f946457cc844b9883a853a4784ba1ff
ElAmirr <othmaniamir41@gmail.com>
2021-05-07 00:59:41 +0200

    use len

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if my_string is none:
+        return
     new_string = ""
-    if my_string:
-        for elm in my_string:
-            if elm != 'c' or elm != 'C':
-                new_string += elm
-        return new_string
-    return
+    length = len(new_string)
+    for elm in range(length):
+        new_string += new_string[elm]
+    return new_string
