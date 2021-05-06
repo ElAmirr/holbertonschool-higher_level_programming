c12a661790933d7e551a2ffd97033f08045ea40b
ElAmirr <othmaniamir41@gmail.com>
2021-05-07 01:13:03 +0200

    return fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,4 +7,4 @@ def no_c(my_string):
     for elm in range(length):
         if my_string[elm] != 'c' and my_string[elm] != 'C':
             new_string += my_string[elm]
-        return new_string
+    return new_string
