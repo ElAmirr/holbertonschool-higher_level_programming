8b6d277ac728eeae6742f3e179dbe5cfca670e2a
ElAmirr <othmaniamir41@gmail.com>
2021-05-09 15:36:31 +0200

    fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = ""
-    for elm in my_list):
+    for elm in my_list:
         new_list.append(False if elm % 2 else True)
     return new_list
