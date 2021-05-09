8f333ac8798228278f8d7abec950d2f0d0af6757
ElAmirr <othmaniamir41@gmail.com>
2021-05-09 15:31:11 +0200

    add else statement

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,11 +7,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_a = (0, 0)
         else:
             tuple_a = (tuple_a[0], 0)
+    else:
+        tuple_a = (tuple_a[0], tuple_a[1])
     if len_b < 2:
         if tuple_b == 0:
             tuple_b = (0, 0)
         else:
             tuple_b = (tuple_b[0], 0)
+    else:
+        tuple_b = (tuple_b[0], tuple_b[1])
     a = tuple_a[0] + tuple_b[0]
     b = tuple_a[1] + tuple_b[1]
     tuple_c = (a, b)
