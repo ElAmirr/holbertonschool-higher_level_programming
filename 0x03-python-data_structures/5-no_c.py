730e6229eb84791e3729a7271197449f8c108529
ElAmirr <othmaniamir41@gmail.com>
2021-05-24 23:09:14 +0200

    reset

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-        return
     for i in range(len(my_string) - 2):
         if my_string[0] == 'c' or my_string[i] == 'C':
             my_string = my_string[:i] + "" + my_string[i+1:]
