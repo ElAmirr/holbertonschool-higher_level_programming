4705438dac768cc77679bb502a74ca9363ff7fe0
ElAmirr <othmaniamir41@gmail.com>
2021-05-24 23:17:27 +0200

    test

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(: len(my_string)):
-        if my_string[0] == 'c' or my_string[i] == 'C':
+    for i in range(len(my_string) - 2):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
             my_string = my_string[:i] + "" + my_string[i+1:]
-    return my_string
+    return (my_string)
