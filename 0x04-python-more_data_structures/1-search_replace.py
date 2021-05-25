8fcf0bf07163dad7077c164aa4dd9a6ee5e1629d
ElAmirr <othmaniamir41@gmail.com>
2021-05-25 17:01:08 +0200

    fix return value

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python
 def search_replace(my_list, search, replace):
-    if len(my_list) > 0:
-        new_list = []
-        for i in range(len(my_list)):
-            if my_list[i] == search:
-                new_list.insert(i,  replace)
-            else:
-                new_list.insert(i, my_list[i])
-        return new_list
+    return [replace if i == search else i for i in my_list]
