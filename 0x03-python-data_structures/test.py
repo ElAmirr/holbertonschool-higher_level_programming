9fa0f13a7973d2066dfb5a4561737b3ef0412f75
ElAmirr <othmaniamir41@gmail.com>
2021-05-25 11:57:44 +0200

    chmod

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -1,11 +1,3 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    i = 0
-    new = ""
-    for char in my_string:
-        if char == 'c' or char == 'C':
-            continue
-        else:
-            new[i] = new[:i] + "" + char
-            i = i + 1
-        return new
+str = "holberton school"
+for i in range(len(str)-2):
+    print(i)
