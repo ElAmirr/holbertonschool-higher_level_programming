ddb886b3cbc34c801ecd332bdca4616f5f7e99d7
ElAmirr <othmaniamir41@gmail.com>
2021-05-25 16:14:41 +0200

    push

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
 
     if len(matrix) > 0:
-        for elm in matrix[:]:
-            new_matrix.append(list(map(lambda x: x ** 2,  elm)))
+        for elems in matrix[:]:
+            new_matrix.append(list(map(lambda x: x ** 2, elems)))
+
     return new_matrix
