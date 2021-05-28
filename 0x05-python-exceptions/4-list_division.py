b132d5e6c1a6412c117b6b43a075aa0056586f0d
ElAmirr <othmaniamir41@gmail.com>
2021-05-28 15:34:30 +0200

    test

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_tab = []
+    for i, j in zip(my_list_1, my_list_2):
+        try:
+            result = i / j
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new_tab.append(result)
+    return new_tab
