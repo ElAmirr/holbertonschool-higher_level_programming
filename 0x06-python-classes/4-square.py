f3b456d319a6e1e296e3711a570a08072a203b98
ElAmirr <othmaniamir41@gmail.com>
2021-06-01 13:02:02 +0200

    fix the variable

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,9 +19,9 @@ class Square:
     @size.setter
     def size(self, value):
         """sets the size with safe assignement"""
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if(size < 0):
+        if(value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
 
