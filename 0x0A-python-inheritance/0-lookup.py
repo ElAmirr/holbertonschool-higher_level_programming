4d85559f76678107a02ef599b9c0696edb25a0c5
ElAmirr <othmaniamir41@gmail.com>
2021-06-12 20:18:27 +0200

    fast

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,4 +7,4 @@ Program that Lookup all attributes and methods of an object
 def lookup(obj):
     """function that returns the list of available attributes
     and methods of an object """
-    return (dir(object))
+    return (dir(obj))
