b683ac89d27ecbe36ea82bde2c0121cf1c2e1ebf
ElAmirr <othmaniamir41@gmail.com>
2021-06-15 18:08:23 +0200

    change the f string

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,7 +7,7 @@ Program that prints the name with edge cases
 def say_my_name(first_name, last_name=""):
     """ Function that prints our name """
     if not (isinstance(first_name, str)):
-        raise TypeError('first_name must be a string')
+        raise TypeError("first_name must be a string")
     if not (isinstance(last_name, str)):
         raise TypeError("last_name must be a string")
-    print(f'My name is {first_name} {last_name}')
+    print("My name is {:s} {:s}".format(first_name, last_name))
