cd80f842942b9327ff4bdc201cb9b7afc6d53a68
ElAmirr <othmaniamir41@gmail.com>
2021-06-26 16:09:46 +0200

    second task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,3 +21,34 @@ class Rectangle(Base):
 
     def width(self, value):
         self.__width = value
+
+    @property
+    def height(self):
+        """ Retrieb the height of rectangle """
+        return(self.__height)
+
+    @height.setter
+    """ set passet private attribute of height """
+
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Retrieve private instance attribute x """
+        return(self.__x)
+
+    @x.setter
+    def x(self, value):
+        """ set passet private instance attribute x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Retrieve private instance attribute y"""
+        return(self.__y)
+
+    @y.setter
+    def y(self, value):
+        """ set passet private instance attribute y"""
+        self.__y = Value
