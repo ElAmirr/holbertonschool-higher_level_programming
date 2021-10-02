4058a9ed72e3d2770086ff187b813ed86d912f41
ElAmirr <othmaniamir41@gmail.com>
2021-10-02 17:18:33 +0200

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
+if (process.argv.length === 3) {
+  const fs = require('fs');
 
-const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) console.log(err);
-  else console.log(data);
-});
+  fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
+}
