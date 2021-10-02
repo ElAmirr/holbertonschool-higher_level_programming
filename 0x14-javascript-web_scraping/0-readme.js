52a7ac327bc92c6f0346d36bf7f6048f08f5d868
ElAmirr <othmaniamir41@gmail.com>
2021-10-02 17:28:51 +0200

    undo

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 3) {
-  const fs = require('fs');
 
-  fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-  });
-}
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
+});
